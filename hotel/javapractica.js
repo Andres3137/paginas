f071e0e85b21da09e5c43471a77c4e5f975d2795
Andres3137 <andres06felipe20@gmail.com>
2024-12-21 18:41:36 -0500

    Add files via upload

---
+++ hotel/javapractica.js
@@ -0,0 +1,48 @@
+/*let a = 5;
+let b = 2;
+console.log("suma " + (a + b));
+console.log("resta " + (a - b));
+console.log("multiplicacion " + a * b);
+console.log("division " + a / b);
+
+let nombre = prompt("¿como te llamas?");
+console.log(
+  "hola " + nombre + " Bienvenido al mundo de felipe aprendiendo java script"
+);
+
+let edad = prompt("Eres mayor de 18 años");
+
+if (edad === "si") {
+  console.log("Eres mayor de edad");
+} else if (edad === "no") {
+  console.log("Eres menor de edad");
+} else {
+  console.log("no entiendo tu respuesta");
+}
+
+Escribe un programa que:
+
+Pida al usuario un número.
+Determine si el número es positivo, negativo o cero.
+Muestra el resultado en la consola.
+
+let numero = prompt("ingrese su numero ");
+
+if (numero > 0) {
+  console.log("El numero es positivo");
+} else if (numero < 0) {
+  console.log("el numero es negativo ");
+} else {
+  console.log("el numero es cero");
+}
+  Pida al usuario dos números y una operación (suma, resta, multiplicación o división).
+Use if y else if para realizar la operación elegida.
+Muestra el resultado en la consola.*/
+
+let numero = prompt("ingrese el numero");
+let operador = prompt("ingrese el operador suma,resta,multiplicacion,division");
+if ((numero = operador)) {
+  console.log(numero + operador);
+} else if ((numero = operador)) {
+  console.log(numero - operador);
+}
